92479e97c9dbf874cfa57d6c9905eb8334444811
KaMi lml <kami@dotastore.pe>
2017-03-27 06:21:11 -0500

    cardsbank responsive + caps n provincia

--- src/components/cuentas/Cardbank.js
+++ src/components/cuentas/Cardbank.js
@@ -5,7 +5,14 @@ import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColu
 import {bankimages} from './db'
 
 const styles = {
-  margin: 10,
+  card1: {
+    margin: '2%',
+    width: '46%',
+    float: 'left'
+  },
+  card2: {
+    margin: '4%'
+  },
   titlestyle: {
     fontSize: 17
   },
@@ -15,6 +22,11 @@ const styles = {
   cardtext: {
     padding: 0,
     paddingLeft: 20
+  },
+  tabla: {
+    paddingLeft: 10,
+    margin: 0,
+    height: 10
   }
 }
 
@@ -27,9 +39,16 @@ export default class CardExampleControlled extends Component {
     let comision = this.props.comision;
     let cuenta = this.props.cuenta;
     let img = bankimages[this.props.banco];
+    let estilocard = [];
+
+    if (screen.width > 640) {
+        estilocard = styles.card1;
+    } else {
+        estilocard = styles.card2;
+    }
 
     return (
-      <Card style={styles}>
+      <Card style={estilocard}>
         <CardHeader
           title={cuenta}
           subtitle={banco+' - '+provincia}
@@ -38,27 +57,27 @@ export default class CardExampleControlled extends Component {
           titleStyle={styles.titlestyle}
         />
         <CardText style={styles.cardtext}>
-          * Revisa la tabla de comisiones para tu provincia.
+          * Revisa la tabla de comisiones para tu provincia.<br/><br/>
         </CardText>
         <Table>
           <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-            <TableRow>
-              <TableHeaderColumn>Método de pago</TableHeaderColumn>
-              <TableHeaderColumn>Comisión</TableHeaderColumn>
+            <TableRow style={{height: 30, background: '#f5f5f5'}}>
+              <TableHeaderColumn style={styles.tabla}>Método de pago</TableHeaderColumn>
+              <TableHeaderColumn style={styles.tabla}>Comisión</TableHeaderColumn>
             </TableRow>
           </TableHeader>
           <TableBody displayRowCheckbox={false}>
-            <TableRow>
-              <TableRowColumn>Agente</TableRowColumn>
-              <TableRowColumn>S/.{comision.A}</TableRowColumn>
+            <TableRow style={{height: 30}}>
+              <TableRowColumn style={styles.tabla}>Agente</TableRowColumn>
+              <TableRowColumn style={styles.tabla}>S/.{comision.A}</TableRowColumn>
             </TableRow>
-            <TableRow>
-              <TableRowColumn>Ventanilla</TableRowColumn>
-              <TableRowColumn>S/.{comision.D}</TableRowColumn>
+            <TableRow style={{height: 30}}>
+              <TableRowColumn style={styles.tabla}>Ventanilla</TableRowColumn>
+              <TableRowColumn style={styles.tabla}>S/.{comision.D}</TableRowColumn>
             </TableRow>
-            <TableRow>
-              <TableRowColumn>Transferencia</TableRowColumn>
-              <TableRowColumn>S/.0</TableRowColumn>
+            <TableRow style={{height: 30}}>
+              <TableRowColumn style={styles.tabla}>Transferencia</TableRowColumn>
+              <TableRowColumn style={styles.tabla}>S/.0</TableRowColumn>
             </TableRow>
           </TableBody>
         </Table>
--- src/components/cuentas/Cuentas.js
+++ src/components/cuentas/Cuentas.js
@@ -4,14 +4,18 @@ import {Helmet} from "react-helmet"
 import Titulo from '../Titulo.js'
 import Divider from 'material-ui/Divider'
 import AutoComplete from 'material-ui/AutoComplete'
-import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton'
 import Cardbank from './Cardbank'
+import Checkbox from 'material-ui/Checkbox'
 
 import {dbprovincia, dbbanco} from './db'
 
 const styles = {
-  radioButton: {
-    margin: 5,
+  check: {
+    marginLeft: 25,
+    marginTop: 10,
+    marginBottom: 10,
+    width: '80%',
+    overflow: 'hidden'
   },
   paper: {
     margin: 15,
@@ -24,11 +28,17 @@ export default class DividerComponent extends Component {
 
   state = {
     provincia: '',
-    banco: ''
+    banco: {
+      BCP: false,
+      Interbank: false,
+      BBVA: false,
+      Scotiabank: false
+    }
   }
 
   handleProvincia = (value) => {
-    this.setState({provincia: value});
+    let provincia = value.charAt(0).toUpperCase()+value.slice(1);
+    this.setState({provincia: provincia});
   }
 
   handleBanco = (event, value) => {
@@ -39,58 +49,96 @@ export default class DividerComponent extends Component {
 
     let provincia = this.state.provincia;
     let banco = this.state.banco;
-    let msg = '';
-    let cuenta = '';
+    let msg = '';;
+    let msg_bcp = '';
+    let msg_inter = '';
+    let msg_bbva = '';
+    let msg_scotia = '';
     let comision = {'A':0, 'D':0};
 
-    if (provincia !== '' && banco !== '' && dbprovincia.indexOf(provincia) > -1) {
+    if ((banco.BCP === true || banco.Interbank === true || banco.BBVA === true || banco.Scotiabank === true ) && dbprovincia.indexOf(provincia) > -1) {
 
-      switch(banco) {
-        case 'Scotiabank':
-          cuenta = '069-7401578';
-          break;
-        case 'BBVA':
-          cuenta = '0257-02-00154221';
-          if (provincia !== 'Lima') comision = {'A':7.5, 'D':7.5};
-          break;
-        case 'BCP':
-          switch (dbbanco.BCP[provincia]) {
-            case '194-20691110-0-10':
-              cuenta = dbbanco.BCP.Lima;
-              comision = {'A':0, 'D':5};
-              break;
-            case undefined:
-              cuenta = dbbanco.BCP.Lima;
-              comision = {'A':7.5, 'D':12.5};
-              break;
-            default:
-              cuenta = dbbanco.BCP[provincia];
-              comision = {'A':0, 'D':9};
-          }
-          break;
-        case 'Interbank':
-          switch (dbbanco.Interbank[provincia]) {
-            case '091-3057202430':
-              cuenta = dbbanco.Interbank.Lima;
-              comision = {'A':0, 'D':7};
-              break;
-            case undefined:
-              cuenta = dbbanco.Interbank.Lima;
-              comision = {'A':0, 'D':9.5};
-              break;
-            default:
-              cuenta = dbbanco.Interbank[provincia];
-          }
-          break;
-        default:
-      }
-
-      msg = <Cardbank
+      if (banco.BCP === true){
+        switch (dbbanco.BCP[provincia]) {
+          case '194-20691110-0-10':
+            msg_bcp = <Cardbank
               provincia={provincia}
-              banco={banco}
+              banco={'BCP'}
+              comision={{'A':0, 'D':5}}
+              cuenta={dbbanco.BCP.Lima}
+            />;
+            break;
+          case undefined:
+            msg_bcp = <Cardbank
+              provincia={provincia}
+              banco={'BCP'}
+              comision={{'A':7.5, 'D':12.5}}
+              cuenta={dbbanco.BCP.Lima}
+            />;
+            break;
+          default:
+            msg_bcp = <Cardbank
+              provincia={provincia}
+              banco={'BCP'}
+              comision={{'A':0, 'D':9}}
+              cuenta={dbbanco.BCP[provincia]}
+            />;
+        }
+      }
+      if (banco.Interbank === true){
+        switch (dbbanco.Interbank[provincia]) {
+          case '091-3057202430':
+            msg_inter = <Cardbank
+              provincia={provincia}
+              banco={'Interbank'}
+              comision={{'A':0, 'D':7}}
+              cuenta={dbbanco.Interbank.Lima}
+            />;
+            break;
+          case undefined:
+            msg_inter = <Cardbank
+              provincia={provincia}
+              banco={'Interbank'}
+              comision={{'A':0, 'D':9.5}}
+              cuenta={dbbanco.Interbank.Lima}
+            />;
+            break;
+          default:
+            msg_inter = <Cardbank
+              provincia={provincia}
+              banco={'Interbank'}
               comision={comision}
-              cuenta={cuenta}
-            />
+              cuenta={dbbanco.Interbank[provincia]}
+            />;
+        }
+      }
+      if (banco.BBVA === true){
+        if (provincia === 'Lima') {
+          msg_bbva = <Cardbank
+            provincia={provincia}
+            banco={'BBVA'}
+            comision={comision}
+            cuenta={'0257-02-00154221'}
+          />;
+        } else {
+          msg_bbva = <Cardbank
+            provincia={provincia}
+            banco={'BBVA'}
+            comision={{'A':7.5, 'D':7.5}}
+            cuenta={'0257-02-00154221'}
+          />;
+        }
+      }
+      if (banco.Scotiabank === true){
+        msg_scotia = <Cardbank
+          provincia={provincia}
+          banco={'Scotiabank'}
+          comision={comision}
+          cuenta={'069-7401578'}
+        />;
+      }
+    } else {
+      msg = <center><br/>* Escribe arriba el nombre de tu provincia<br/>y <b>dale click</b> en la lista</center>;
     }
 
     return (
@@ -104,45 +152,74 @@ export default class DividerComponent extends Component {
         </Helmet>
         
         <Titulo tab="cuentas" />
-          <AutoComplete
-            floatingLabelText="Provincia donde pagarás"
-            filter={AutoComplete.caseInsensitiveFilter}
-            dataSource={dbprovincia}
-            maxSearchResults={5}
-            onUpdateInput={this.handleProvincia}
-            openOnFocus={true}
-            style={styles.paper}
-            fullWidth={true}
-          />
-          <Divider />
-          <RadioButtonGroup
-            onChange={this.handleBanco}
-            name="selectbancos"
-            style={styles.paper}
-          >
-            <RadioButton
-              value="BCP"
-              label="BCP"
-              style={styles.radioButton}
-            />
-            <RadioButton
-              value="Interbank"
-              label="Interbank"
-              style={styles.radioButton}
-            />
-            <RadioButton
-              value="BBVA"
-              label="BBVA"
-              style={styles.radioButton}
-            />
-            <RadioButton
-              value="Scotiabank"
-              label="Scotiabank"
-              style={styles.radioButton}
-            />
-          </RadioButtonGroup>
-          <Divider />
-          {msg}
+        <center>
+        <div style={{textAlign: 'left', maxWidth: 990}}>
+        <AutoComplete
+          floatingLabelText="Provincia donde pagarás"
+          filter={AutoComplete.caseInsensitiveFilter}
+          dataSource={dbprovincia}
+          maxSearchResults={5}
+          onUpdateInput={this.handleProvincia}
+          openOnFocus={true}
+          style={styles.paper}
+          fullWidth={true}
+        />
+        <Divider />
+        <Checkbox
+          style={styles.check}
+          label="BCP"
+          onCheck={(e, i) => this.setState({
+            banco:{
+              BCP: i,
+              Interbank: this.state.banco.Interbank,
+              BBVA: this.state.banco.BBVA,
+              Scotiabank: this.state.banco.Scotiabank
+            }
+          })}
+        />
+        <Checkbox
+          style={styles.check}
+          label="Interbank"
+          onCheck={(e, i) => this.setState({
+            banco:{
+              BCP: this.state.banco.BCP,
+              Interbank: i,
+              BBVA: this.state.banco.BBVA,
+              Scotiabank: this.state.banco.Scotiabank
+            }
+          })}
+        />
+        <Checkbox
+          style={styles.check}
+          label="BBVA"
+          onCheck={(e, i) => this.setState({
+            banco:{
+              BCP: this.state.banco.BCP,
+              Interbank: this.state.banco.Interbank,
+              BBVA: i,
+              Scotiabank: this.state.banco.Scotiabank
+            }
+          })}
+        />
+        <Checkbox
+          style={styles.check}
+          label="Scotiabank"
+          onCheck={(e, i) => this.setState({
+            banco:{
+              BCP: this.state.banco.BCP,
+              Interbank: this.state.banco.Interbank,
+              BBVA: this.state.banco.BBVA,
+              Scotiabank: i
+            }
+          })}
+        />
+        <Divider />
+        {msg_bcp}
+        {msg_inter}
+        {msg_bbva}
+        {msg_scotia}
+        {msg}
+        </div></center>
       </div>
     );
   }
